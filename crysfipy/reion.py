a35ebba39793058ff098b2484a36190acd8e25dc
oberth-effect <stepan.venclik@gmail.com>
2023-04-06 19:12:18 +0200

    some notes

--- crysfipy/reion.py
+++ crysfipy/reion.py
@@ -20,6 +20,7 @@ from numpy import diag, conj, transpose, dot
 from numpy.linalg import eig
 import numbers
 
+
 class cfpars:
     """Class representing set of crystal field parameters.
 
@@ -69,58 +70,57 @@ class cfpars:
         B64 = -21.0000
 
     """
-    
+
     pars = {
-    "c": ["cubic", ["B40", "B60", "B44", "B64"]],            
-    "h": ["hexagonal", ["B20", "B40", "B60", "B66"]],              
-    "t": ["tetragonal", ["B20", "B40", "B44", "B60", "B64"]],            
-    "o": ["orthorombic", ["B20", "B22", "B40", "B42", "B44", "B60", "B62", "B64", "B66"]],  
+        "c": ["cubic", ["B40", "B60", "B44", "B64"]],
+        "h": ["hexagonal", ["B20", "B40", "B60", "B66"]],
+        "t": ["tetragonal", ["B20", "B40", "B44", "B60", "B64"]],
+        "o": ["orthorombic", ["B20", "B22", "B40", "B42", "B44", "B60", "B62", "B64", "B66"]],
     }
-    
+
     def __init__(self, *args, **kwargs):
         skipNonnamed = False
-        self.sym = ""  #orthorombic symetry (=no constrains)
-        #clear all params
+        self.sym = ""  # orthorombic symetry (=no constrains)
+        # clear all params
         for name in self.pars["o"][1]: self._asignParameter(name, 0)
-        #check named args
+        # check named args
         for name, value in kwargs.items():
-            #check symetry constrain
+            # check symetry constrain
             if name == "sym":
-                self.sym = self._asignSymmetry(value)    
-            if name[0] == "B":  #it is a crystal field
+                self.sym = self._asignSymmetry(value)
+            if name[0] == "B":  # it is a crystal field
                 skipNonnamed = True
                 self._asignParameter(name, value)
-        #check non named args
+        # check non named args
         if self.sym == "":
             for value in args:
                 if isinstance(value, str):
                     self.sym = self._asignSymmetry(value)
-                    break  #just consider first string in list
-        if self.sym == "": self.sym = "o" #default
+                    break  # just consider first string in list
+        if self.sym == "": self.sym = "o"  # default
         if not skipNonnamed:
-            #read
+            # read
             i = 0
             for value in args:
                 if isinstance(value, numbers.Real):
                     if i >= len(self.pars[self.sym][1]): break
                     self._asignParameter(self.pars[self.sym][1][i], value)
-                    i+=1
-        #do symmetry magic
+                    i += 1
+        # do symmetry magic
         if self.sym == "c":
             self.B44 = 5 * self.B40;
             self.B64 = -21 * self.B60;
-            
+
     def __str__(self):
         ret = "Set of CF parameters for %s symmetry:" % (self.pars[self.sym][0])
         for name in self.pars[self.sym][1]: ret += self._printParameter(name) + "\n"
         return ret
-    
-            
+
     def _asignSymmetry(self, inp):
-        if inp[0] == "t" or  inp[0] == "h" or  inp[0] == "c":
+        if inp[0] == "t" or inp[0] == "h" or inp[0] == "c":
             return inp[0]
         return ""
-                  
+
     def _asignParameter(self, name, value):
         if name == "B20": self.B20 = value
         if name == "B22": self.B22 = value
@@ -131,7 +131,7 @@ class cfpars:
         if name == "B62": self.B62 = value
         if name == "B64": self.B64 = value
         if name == "B66": self.B66 = value
-        
+
     def _printParameter(self, name):
         if name == "B20": return "%s = %.4f" % (name, self.B20)
         if name == "B22": return "%s = %.4f" % (name, self.B22)
@@ -142,8 +142,6 @@ class cfpars:
         if name == "B62": return "%s = %.4f" % (name, self.B62)
         if name == "B64": return "%s = %.4f" % (name, self.B64)
         if name == "B66": return "%s = %.4f" % (name, self.B66)
-
-
 
 
 class re:
@@ -165,102 +163,100 @@ class re:
         E(1) =	3600.0000	 4fold-degenerated
     """
 
-    def __init__(self, name, field, cfp, calculate = True):
+    def __init__(self, name, field, cfp, calculate=True):
         self.name = name
         self.field = field
         if type(cfp) is list:
             cfp = cfpars(*cfp)
         self.cfp = cfp
-        
+
         self.H = np.array(field)
         self.H_size = np.sqrt(dot(self.H, self.H.conj().transpose()))
         if self.H_size > 0:
             self.H_direction = self.H / self.H_size
         if (calculate):
             self.getlevels()
-    
-    
-    
+
     def getlevels(self):
         """Calculate degeneracy of the levels and sort the matrix"""
 
         self._calculate()
-        #orthogonalization not needed?
-        #self.rawev,R = np.linalg.qr(self.rawev)
-        
-        #change the sign to be positive :)
+        # orthogonalization not needed?
+        # self.rawev,R = np.linalg.qr(self.rawev)
+
+        # change the sign to be positive :)
         self.rawev = self.rawev * np.sign(np.sum(self.rawev, axis=0))
 
-        self.energy = self.rawenergy - min(self.rawenergy)     # shift to zero level	
-        #sorting
-        self.ev = self.rawev[:,self.rawenergy.argsort()]
+        self.energy = self.rawenergy - min(self.rawenergy)  # shift to zero level
+        # sorting
+        self.ev = self.rawev[:, self.rawenergy.argsort()]
         self.energy = np.sort(self.energy)
-        #get sorted Jx,Jz and Jz 
+        # get sorted Jx,Jz and Jz
         self.Jz = dot(dot(self.ev.conj().transpose(), J_z(self.J)), self.ev)
         self.Jx = dot(dot(self.ev.conj().transpose(), J_x(self.J)), self.ev)
         self.Jy = dot(dot(self.ev.conj().transpose(), J_y(self.J)), self.ev)
-        #calculate J^2 matrices
+        # calculate J^2 matrices
         self.Jx2 = np.square(np.abs(self.Jx))
         self.Jy2 = np.square(np.abs(self.Jy))
         self.Jz2 = np.square(np.abs(self.Jz))
 
-        #calculate degeneracy
+        # calculate degeneracy
         deg_e = []
         levels = 0
         deg_e.append([self.energy[0], 0])
         for x in self.energy:
             if not np.isclose(deg_e[levels][0], x):
-                levels+=1
+                levels += 1
                 deg_e.append([x, 1])
             else:
                 deg_e[levels][1] += 1
-        
+
         levels += 1  # started at zero
-        
-        #empty degenerate level transition matrices
-        self.deg_Jx2 = np.zeros((levels,levels))
-        self.deg_Jy2 = np.zeros((levels,levels))
-        self.deg_Jz2 = np.zeros((levels,levels))
-        #sum degenerated levels
+
+        # empty degenerate level transition matrices
+        self.deg_Jx2 = np.zeros((levels, levels))
+        self.deg_Jy2 = np.zeros((levels, levels))
+        self.deg_Jz2 = np.zeros((levels, levels))
+        # sum degenerated levels
         u = 0
         v = 0
         for i, x in enumerate(deg_e):
             v = 0
             for j, y in enumerate(deg_e):
-                self.deg_Jx2[i,j] = np.sum(self.Jx2[u:u+x[1],v:v+y[1]])
-                self.deg_Jy2[i,j] = np.sum(self.Jy2[u:u+x[1],v:v+y[1]])
-                self.deg_Jz2[i,j] = np.sum(self.Jz2[u:u+x[1],v:v+y[1]])
-                v+=y[1]
-            u+=x[1]        
-            
-        #calculate Jt2 for polycrystal
+                self.deg_Jx2[i, j] = np.sum(self.Jx2[u:u + x[1], v:v + y[1]])
+                self.deg_Jy2[i, j] = np.sum(self.Jy2[u:u + x[1], v:v + y[1]])
+                self.deg_Jz2[i, j] = np.sum(self.Jz2[u:u + x[1], v:v + y[1]])
+                v += y[1]
+            u += x[1]
+
+            # calculate Jt2 for polycrystal
         self.deg_Jt2 = 2.0 / 3 * (self.deg_Jx2 + self.deg_Jy2 + self.deg_Jz2)
-        self.deg_e = np.array(deg_e)   
-        
+        self.deg_e = np.array(deg_e)
+
     def __str__(self):
         """Nice printout of calculated parameters"""
         ret = ""
         ret += "Energy levels:\n"
-        for i,x in enumerate(self.deg_e):
+        for i, x in enumerate(self.deg_e):
             ret += "E({:d}) =\t{:.4f}\t{:2d}fold-degenerated\n".format(i, x[0], int(x[1]))
         return ret
-      
+
     def _calculate(self):
         """Calculates energy splitting in CF potential"""
 
         i = ion(self.name)
         self.J = i.J
-        self.p1 = np.ones((i.J2p1,1), float);      # column vector of ones
-        self.Jx = J_x(i.J); 
+        self.p1 = np.ones((i.J2p1, 1), float);  # column vector of ones
+        self.Jx = J_x(i.J);
         self.Jy = J_y(i.J);
-        self.Jz = J_z(i.J);                        # prepare matrices
+        self.Jz = J_z(i.J);  # prepare matrices
         self.gJ = i.gJ
-        self.moment = np.zeros((i.J2p1,3), float)  # matrix with projection of moments to x, y, z directions for all J2p1 levels
-        
+        self.moment = np.zeros((i.J2p1, 3),
+                               float)  # matrix with projection of moments to x, y, z directions for all J2p1 levels
+
         B = self.cfp
-        
-        
-        self.hamiltonian =  \
+
+        self.hamiltonian = \
             B.B20 * O_20(i.J) + \
             B.B22 * O_22(i.J) + \
             B.B40 * O_40(i.J) + \
@@ -271,23 +267,23 @@ class re:
             B.B64 * O_64(i.J) + \
             B.B66 * O_66(i.J) + \
             C.uB * i.gJ * (self.Jx * self.H[0] + self.Jy * self.H[1] + self.Jz * self.H[2])
-        
+
         E, U = eig(self.hamiltonian);
-        
-        self.rawenergy = np.real(E);  
+
+        self.rawenergy = np.real(E);
         if sum(np.iscomplex(E)) > 0:
             raise ValueError('Final energies are complex!')
-        
-        self.Jz = dot(dot(U.conj().transpose(), self.Jz), U)   # conversion of matrices to the basis of eigenvectors
-        self.Jx = dot(dot(U.conj().transpose(), self.Jx), U)   # it is then easier to calculate <i|J|j>
-        self.Jy = dot(dot(U.conj().transpose(), self.Jy), U)
-        
-        self.moment[:,0] = - i.gJ * np.real(diag(self.Jx))     # calculation of projections of moments for every eigenvector   
-        self.moment[:,1] = - i.gJ * np.real(diag(self.Jy))                      
-        self.moment[:,2] = - i.gJ * np.real(diag(self.Jz))
-        
-        self.rawev = U
 
+        self.Jz = dot(dot(U.conj().transpose(), self.Jz), U)  # conversion of matrices to the basis of eigenvectors
+        self.Jx = dot(dot(U.conj().transpose(), self.Jx), U)  # it is then easier to calculate <i|J|j>
+        self.Jy = dot(dot(U.conj().transpose(), self.Jy), U)
+
+        self.moment[:, 0] = - i.gJ * np.real(
+            diag(self.Jx))  # calculation of projections of moments for every eigenvector
+        self.moment[:, 1] = - i.gJ * np.real(diag(self.Jy))
+        self.moment[:, 2] = - i.gJ * np.real(diag(self.Jz))
+
+        self.rawev = U
 
 
 def _rawneutronint(E, deg, J2, gJ, T):
@@ -300,16 +296,17 @@ def _rawneutronint(E, deg, J2, gJ, T):
         gJ (float): Landé factor
         T (float): temperature in *K*
     """
-    r02 = C.R0 * C.R0  *1e28 # to have value in barn
+    r02 = C.R0 * C.R0 * 1e28  # to have value in barn
     c = np.pi * r02 * gJ * gJ
-    
-    prst = np.exp(-E*C.eV2K/T)
-    Z = sum(prst*deg) #multiply with degeneracy of the level
+
+    prst = np.exp(-E * C.eV2K / T)
+    Z = sum(prst * deg)  # multiply with degeneracy of the level
     prst = prst / Z
-    trans_int = J2 * prst[:, np.newaxis] * c  #transition intensities in barn
+    trans_int = J2 * prst[:, np.newaxis] * c  # transition intensities in barn
     return trans_int
 
-def neutronint(ion, T, direction = "t"):
+
+def neutronint(ion, T, direction="t"):
     """Returns matrix of energy and transition intensity at given temperature
     
     Args:
@@ -323,7 +320,7 @@ def neutronint(ion, T, direction = "t"):
             | z - using :math:`J_z`
         
     """
-    jumps = ion.deg_e[:,0] - ion.deg_e[:,0][:, np.newaxis]
+    jumps = ion.deg_e[:, 0] - ion.deg_e[:, 0][:, np.newaxis]
     jumps = jumps.flatten()
     if direction == "x":
         J2 = ion.deg_Jx2
@@ -333,18 +330,24 @@ def neutronint(ion, T, direction = "t"):
         J2 = ion.deg_Jz2
     else:
         J2 = ion.deg_Jt2
-    tint = _rawneutronint(ion.deg_e[:,0], ion.deg_e[:,1], J2, ion.gJ, T).flatten()
+    tint = _rawneutronint(ion.deg_e[:, 0], ion.deg_e[:, 1], J2, ion.gJ, T).flatten()
     tint = tint[jumps.argsort()]
-    return np.array([np.sort(jumps),tint]) 
+    return np.array([np.sort(jumps), tint])
+
+
+def get_perpendicular_j(ion: re, h, k, l):
+    """Calculates the J_(perpendicular) for specific reciprocal vector (h, k, l)"""
+
 
 def _rawsusceptibility(energy, moment, H_direction, H_size, T):
     """Returns susceptibility calculated for energy levels at given temperature"""
 
-    prst = np.exp(-energy/T)
-    Z = sum(prst);                                    # canonical partition function 
+    prst = np.exp(-energy / T)
+    Z = sum(prst);  # canonical partition function
     prst = prst / Z;
     overal_moment = dot(prst, moment);
     return dot(overal_moment, H_direction.conj().transpose()) / H_size
+
 
 def susceptibility(ion, T):
     """Returns susceptibility calculated for given ion at given temperature"""
